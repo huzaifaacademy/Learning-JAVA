9a957d3b07dbe0ec4ee7b1670811503d3d9a65aa
Muhammad Huzaifa Khan <huzaifaacademy11@gmail.com>
2022-06-09 18:31:08 +0500

    Add assignments of class2

--- class2/src/App.java
+++ class2/src/App.java
@@ -1,5 +1,4 @@
-import java.lang.reflect.Array;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -37,19 +36,61 @@ public class App {
         // } while (j < arr1.length);
 
         // System.out.println(j);
-
-        int num = 2;
         
-        switch (num) {
-            case 1:
-                System.out.println("one");
-                break;
-            case 2:
-                System.out.println("two");
-                break;
-            default:
-                System.out.println("invalid");
-                break;
-        }
+        // SWITCH STRUCTURE
+        // int num = 2;
+        // switch (num) {
+        //     case 1:
+        //         System.out.println("one");
+        //         break;
+        //     case 2:
+        //         System.out.println("two");
+        //         break;
+        //     default:
+        //         System.out.println("invalid");
+        //         break;
+        // }
+
+        // ASSIGNMENT: SEARCHING ALGORITHM
+        // Scanner sc = new Scanner(System.in);
+        // int[] arr = new int[10];
+        
+        // // input array
+        // System.out.println("Enter numbers of array");
+        // for (int i = 0; i < arr.length; i++) {
+        //     arr[i] = sc.nextInt();
+        // }
+
+        // // print array
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.println(arr[i]);
+        // }
+
+        // // input keyword to search
+        // System.out.print("Enter keyword to search :");
+        // int keyword = sc.nextInt();
+        // sc.close();
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     if (arr[i] == keyword) {
+        //         System.out.println(keyword + " found at index " + i);
+        //         break;
+        //     }
+        // }
+
+
+        // // ASSIGNMENT: SUM OF ARRAY
+        // int total_sum = 0;
+        // for (int i = 0; i < arr.length; i++) {
+        //     total_sum += arr[i];
+        // }
+        // System.out.println("SUM OF ARRAY " + total_sum);
+
+        // // ASSIGNMENT: PRODUCT OF ARRAY
+        // int total_prod = 1;
+        // for (int i = 0; i < arr.length; i++) {
+        //     total_prod *= arr[i];
+        // }
+        // System.out.println("PRODUCT OF ARRAY " + total_prod);
     }
 }
